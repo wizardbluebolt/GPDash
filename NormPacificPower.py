e9e569a0003310daa53bc53e056c18054751fba7
Matt Rosen <wizardbluebolt@gmail.com>
2023-06-22 15:45:58 -0700

    Pacific Power normalization script complete.

---
+++ NormPacificPower.py
@@ -0,0 +1,112 @@
+import csv
+import datetime
+import os
+import constants
+
+
+# Return a string describing the range between low_ym and high_ym, where each is a string
+# in the format YYYYMM, with YYYY being the year and MM the month number
+# If low_ym and high_ym are the same value, just return string describing low_ym
+def comp_mon_range(low_ym, high_ym):
+    low = low_ym[0:4] + " " + low_ym[4:6]
+    if low_ym == high_ym:
+        return low
+    else:
+        return low + " to " + high_ym[0:4] + " " + high_ym[4:6]
+
+
+now = datetime.datetime.now()
+fnamenow = now.strftime("%Y-%m-%d T %H%M%S")
+logfname = constants.LOG_DIR + constants.PACIFIC_POWER + "\\" + "Log " + fnamenow + ".txt"
+nl = "\n"
+print("Processing now.  Log file " + logfname)
+with open(logfname, mode="w") as log_file:
+    log_file.write("Reading meter master from " + constants.PACIFIC_POWER_MASTER + nl)
+    meters = dict()
+    ref_in = 0
+    with open(constants.PACIFIC_POWER_MASTER, mode="r") as ref_file:
+        csv_reader = csv.reader(ref_file)
+        for row in csv_reader:
+            ref_in += 1
+            if ref_in > 1:
+                meters.setdefault(row[1], 0)
+    log_file.write("Read " + str(ref_in) + " rows from meter master file." + nl)
+    in_dir = constants.IN_DIR + constants.PACIFIC_POWER
+    out_dir = constants.OUT_DIR + constants.PACIFIC_POWER
+    for in_fname in os.listdir(in_dir):
+        log_file.write("Reading data input from " + in_fname + nl)
+        with open(in_dir + "\\" + in_fname, mode="r") as in_file:
+            csv_reader = csv.reader(in_file)
+            in_count = 0
+            out_count = 0
+            no_meter_num = 0
+            noMeter = dict()
+            out_fname = out_dir + "\\Temp " + fnamenow + ".csv"
+            low_yearmonth = ""
+            high_yearmonth = ""
+            log_file.write("Writing temporary data to " + out_fname + nl)
+            with open(out_fname, mode="w", newline="") as out_file:
+                csv_writer = csv.writer(out_file)
+                csv_writer.writerow(["meter", "yearmonth", "days", "units", "cost"])
+                for row in csv_reader:
+                    in_count += 1
+                    # Skip header row
+                    if in_count > 1:
+                        cust_id = row[0]
+                        acct_num = row[1]
+                        agreement = row[2]
+                        cust_name = row[3]
+                        address = row[4]
+                        csz = row[5]
+                        meter_num = row[6].strip()
+                        year_mon = row[7]
+                        days = row[8]
+                        on_kwh = row[9]
+                        off_kwh = row[10]
+                        kwh = row[11]
+                        on_kw = row[12]
+                        off_kw = row[13]
+                        kw = row[14]
+                        kvar = row[15]
+                        invoice = row[16].replace("$", "").replace(",", "")
+                        if meter_num == "":
+                            no_meter_num += 1
+                        else:
+                            csv_writer.writerow([meter_num, year_mon, days, kwh, invoice])
+                            out_count += 1
+                            if meter_num in meters:
+                                meters[meter_num] = meters[meter_num] + 1
+                            elif meter_num in noMeter:
+                                noMeter[meter_num] = noMeter[meter_num] + 1
+                            else:
+                                noMeter.setdefault(meter_num, 1)
+                        if low_yearmonth == "":
+                            low_yearmonth = year_mon
+                            high_yearmonth = year_mon
+                        else:
+                            if year_mon < low_yearmonth:
+                                low_yearmonth = year_mon
+                            if year_mon > high_yearmonth:
+                                high_yearmonth = year_mon
+        log_file.write("Read " + str(in_count) + " rows from input file" + nl)
+        log_file.write("Wrote " + str(out_count) + " rows to output file" + nl)
+        log_file.write("Number of entries with no meter number " + str(no_meter_num) + nl)
+        if len(noMeter) == 0:
+            log_file.write("All meters in data file have descriptions (no new meters)" + nl)
+        else:
+            log_file.write("The following meters have no description entries (new meters):" + nl)
+            for meter_key in noMeter:
+                log_file.write(meter_key + " has " + str(noMeter[meter_key]) + " data file entries" + nl)
+        target_out = out_dir + "\\" + constants.PACIFIC_POWER + " " + comp_mon_range(low_yearmonth, high_yearmonth) + ".csv"
+        os.rename(out_fname, target_out)
+        log_file.write("Renamed temporary file to " + target_out + nl)
+        log_file.write("Remember to move data file to archive folder to avoid duplicates!" + nl)
+    noData = False
+    log_file.write("The following meters have no data (warning only):" + nl)
+    for meter_key in meters:
+        if meters[meter_key] == 0:
+            noData = True
+            log_file.write(meter_key + nl)
+    if not noData:
+        log_file.write("None" + nl)
+print("Processing complete")
--- constants.py
+++ constants.py
@@ -1,5 +1,50 @@
-BASE_DIR = "D:\\Task Force\\"
-IN_DIR = BASE_DIR + "Source Data\\"
-OUT_DIR = BASE_DIR + "Inputs\\"
+BASE_DIR = "D:\\Task Force\\Dashboard\\Base\\"
+IN_DIR = BASE_DIR + "UtilityIn\\"
+OUT_DIR = BASE_DIR + "UtilityOut\\"
 LOG_DIR = BASE_DIR + "Logs\\"
-REF_DIR = BASE_DIR + "Inputs\\"
+REF_DIR = BASE_DIR + "MasterReference\\"
+PERM_DIR = BASE_DIR + "PermanentData\\"
+DASHBOARD_DIR = BASE_DIR + "DashboardData\\"
+
+PACIFIC_POWER = "PacificPower"
+AVISTA = "Avista"
+PACIFIC_PRIDE = "PacificPride"
+SOLID_WASTE = "SolidWaste"
+
+AVISTA_MASTER = REF_DIR + "AvistaMaster.csv"
+PACIFIC_POWER_MASTER = REF_DIR + "PacificPowerMaster.csv"
+VEHICLES_MASTER = REF_DIR + "VehiclesMaster.csv"
+
+DIESEL_CONSTANTS = REF_DIR + "DieselConstants.csv"
+GASOLINE_CONSTANTS = REF_DIR + "GasolineConstants.csv"
+ELECTRICAL_CONSTANTS = REF_DIR + "ElectricalConstants.csv"
+EMPLOYEE_COMMUTE_CONSTANTS = REF_DIR + "EmployeeCommuteConstants.csv"
+FOREST_SEQ_CONSTANTS = REF_DIR + "ForestSequestrationConstants.csv"
+SOLID_WASTE_CONSTANTS = REF_DIR + "SolidWasteConstants.csv"
+WATER_RESTORE_CONSTANTS = REF_DIR + "WaterRestorationConstants.csv"
+
+# Calculation Constants - The values below are HIGHLY UNLIKELY TO CHANGE!
+mwh_per_kwh = 1000      # Megawatt hours per kilowatt hour
+lb_per_mt = 2204.62     # Pounds per metric ton
+
+cfm_per_therm = 0.0973  # Thousand cubic feet (cfm) per therm of natural gas
+mtco2_per_cfm = 0.05486 # Metric tons of CO2 equivalent per CFM of consumed natural gas
+
+kg_per_mt = 1000            # Kilograms per metric ton
+m3_per_ft3 = 0.028316847    # Cubic meters per cubic foot
+days_per_year = 365.25      # Average days per year
+mt_per_gram = 0.000001      # Metric tons per gram
+
+# Water Restoration Calculation Constants
+digester_ch4_density = 662      # Grams per cubic meter
+digester_ch4_destruct = 0.01    # Methane destruction efficiency from flaring or burning
+ch4_gwp = 25                    # Methane global warming multiplier (impact vs CO2)
+
+# Forest Sequestration Calculation Constants
+forest_seq_factor = -2.23       # Removal in metric tons of carbon per hectare per year
+aw_co2 = 44                     # Atomic weight of CO2
+aw_c = 12                       # Atomic weight of Carbon
+hectare_per_km2 = 100           # Hectares per square kilometer
+
+
+
