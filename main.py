8158e1fcb73e879e6efb9311360de06e71380a52
Matt Rosen <wizardbluebolt@gmail.com>
2023-07-18 07:31:05 -0700

    Provided single point of execution with interactive menu.

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from pandas import DataFrame, Series
-import pandas as pd
+import consolemenu as cm
 
-import constants
 
+choice1 = "Normalize Electrical Utility Data (Pacific Power)"
+choice2 = "Normalize Natural Gas Utility Data (Avista)"
+choice3 = "Normalize Vehicle Fuel Data (Pacific Pride)"
+choice4 = "Generate Emissions Data (Dashboard Data file)"
+choices = [choice1, choice2, choice3, choice4]
+menu = cm.SelectionMenu(choices)
+
+tfname = "go"
+while tfname != "stop":
+    menu.show()
+    selection = menu.selected_item.text
+    if selection == choice1:
+        tfname = 'NormPacificPower.py'
+    elif selection == choice2:
+        tfname = 'NormAvista.py'
+    elif selection == choice3:
+        tfname = 'NormPacificPride.py'
+    elif selection == choice4:
+        tfname = 'ComputeEmissions.py'
+    else:
+        tfname = "stop"
+    if tfname != "stop":
+        with open(tfname, mode="r") as subfile:
+            exec(subfile.read())
